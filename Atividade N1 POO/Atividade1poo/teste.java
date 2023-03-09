eb95bbc287f51b2089c3d78adbeac760eca44313
Rafael Santos <102121949+rafasantos12@users.noreply.github.com>
2023-03-09 10:50:52 -0300

    Add files via upload

---
+++ Atividade N1 POO/Atividade1poo/teste.java
@@ -0,0 +1,8 @@
+public class teste {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Rafael", 23, "ADS");
+        aluno1.apresentar();
+        aluno1.estudar();
+    }
+    
+}
