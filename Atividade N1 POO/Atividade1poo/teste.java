edaae4246286db80728a3bd58ead536a2d287d69
Rafael Santos <102121949+rafasantos12@users.noreply.github.com>
2023-03-13 16:04:25 -0400

    Delete Atividade N1 POO/Atividade1poo directory

--- Atividade N1 POO/Atividade1poo/teste.java
+++
@@ -1,8 +0,0 @@
-public class teste {
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("Rafael", 23, "ADS");
-        aluno1.apresentar();
-        aluno1.estudar();
-    }
-    
-}
